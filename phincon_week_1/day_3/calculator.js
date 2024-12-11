22abfc131e1c55daa0003584ad5b1a991256e363
RofiDay <rofidailamy@gmail.com>
2024-12-11 16:04:23 +0700

    feat : "calculator"

---
+++ phincon_week_1/day_3/calculator.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+  return a + b;
+}
+function substract(a, b) {
+  return a - b;
+}
+function multiplier(a, b) {
+  return a * b;
+}
+function divider(a, b) {
+  return a / b;
+}
+let resultSum = sum(10, 10);
+let resultSubstract = substract(10, 10);
+let resultMultiplier = multiplier(10, 10);
+let resultDivider = divider(10, 10);
+
+console.log(resultSum);
+console.log(resultSubstract);
+console.log(resultMultiplier);
+console.log(resultDivider);
